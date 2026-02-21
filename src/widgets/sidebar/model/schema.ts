db3fb35d9e43f76da858a7d5503d51893fc7d049
jooyong-boo <qnwndyd159@naver.com>
2026-02-22 00:56:14 +0900

    edit: zod 수정

--- src/widgets/sidebar/model/schema.ts
+++ src/widgets/sidebar/model/schema.ts
@@ -2,9 +2,8 @@ import { z } from 'zod';
 
 export const newsletterSchema = z.object({
   email: z
-    .string()
-    .min(1, '이메일을 입력해주세요.')
-    .email('올바른 이메일 형식을 입력해주세요.'),
+    .email('올바른 이메일 형식을 입력해주세요.')
+    .min(1, '이메일을 입력해주세요.'),
 });
 
 export type NewsletterFormData = z.infer<typeof newsletterSchema>;
