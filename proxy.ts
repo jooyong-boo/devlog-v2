d838ebe42ff12653e2a54ae584c7e39dbeeebc1f
jooyong-boo <qnwndyd159@naver.com>
2026-02-25 02:42:06 +0900

    fix: proxy 함수 export 형식으로 빌드 오류 수정

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,13 @@
-export { auth as default } from './auth';
+import type { NextFetchEvent, NextRequest } from 'next/server';
+import { auth } from './auth';
+
+const authProxy = auth((_request, _event: NextFetchEvent) => undefined);
+
+export function proxy(request: NextRequest, event: NextFetchEvent) {
+  return authProxy(request, event);
+}
+
+export default proxy;
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
